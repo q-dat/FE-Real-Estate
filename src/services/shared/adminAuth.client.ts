63a42ee30a432e0e37639756203660761dcfadf7
q-dat <quocdatstore79@gmail.com>
2026-01-05 23:21:24 +0700

    chore: getAuthSession

--- src/services/shared/adminAuth.client.ts
+++ src/services/shared/adminAuth.client.ts
@@ -1,10 +1,11 @@
 'use client';
 import { AuthSession } from "@/types/type/auth/auth";
+import { getAuthToken } from "./getAuthToken";
 
 export function getAdminToken(): string {
   if (typeof window === 'undefined') return '';
 
-  const token = localStorage.getItem('token');
+  const token = getAuthToken();
   return token ?? '';
 }
 
@@ -19,7 +20,7 @@ export function requireAdminToken(): string {
 export function getAuthSession(): AuthSession | null {
   if (typeof window === 'undefined') return null;
 
-  const token = localStorage.getItem('token');
+  const token = getAuthToken();
   const role = localStorage.getItem('role') as AuthSession['role'] | null;
 
   if (!token || !role) return null;
